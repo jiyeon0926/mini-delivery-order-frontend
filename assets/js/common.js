e91a34735bd6822dcf90e6ec5c7dcaac47637867
jiyeon0926 <yeoni9094@naver.com>
2026-04-24 21:42:27 +0900

    feat: 로그인 상태에 따른 UI 제어 common.js 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,16 @@
+// 로그인 시 로그인 → 로그아웃 버튼으로 교체
+function updateAuthUI() {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    $(".login-menu").addClass("d-none");
+    $(".logout-menu").removeClass("d-none");
+  } else if (!token) {
+    $(".login-menu").removeClass("d-none");
+    $(".logout-menu").addClass("d-none");
+  }
+}
+
 // 로그아웃 버튼 클릭 시 동작
 $("#logoutBtn").on("click", function () {
   logoutAPI()
